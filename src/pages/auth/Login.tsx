7d6d70c6f8534b8e4381926b082e349a745bfb77
davydocsurg <davydocsurg@gmail.com>
2023-01-25 13:53:51 +0100

    complete user registration with email and password #2

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,20 +1,47 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../../firebase";
 import { config } from "../../config";
 
 const Login = (): React.ReactElement => {
     const [err, setErr] = useState(false);
+    const [fields, setFields] = useState({
+        email: "",
+        password: "",
+    });
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            await signInWithEmailAndPassword(
+                auth,
+                fields.email,
+                fields.password
+            );
+            navigate("/");
+        } catch (err) {
+            setErr(true);
+        }
+    };
 
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">{config.appName}</span>
                 <span className="title">Login</span>
-                <form onSubmit={() => console.log("")}>
+                <form onSubmit={handleSubmit}>
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
                     <button>Sign in</button>
-                    {err && <span>Something went wrong</span>}
+                    {err && (
+                        <span>
+                            Something went wrong. Check your details and try
+                            again!
+                        </span>
+                    )}
                 </form>
                 <p>
                     You don't have an account?{" "}
