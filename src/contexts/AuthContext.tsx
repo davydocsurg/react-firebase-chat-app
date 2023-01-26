09c93c0ce8af6170f46f2da823bbddbbfb65b0ff
davydocsurg <davydocsurg@gmail.com>
2023-01-26 10:53:53 +0100

    define auth context types

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,17 +2,47 @@ import { onAuthStateChanged } from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
 import { auth } from "../../firebase";
 
-export const AuthContext = createContext({
-    authUser: {},
+type AuthContextType = {
+    accessToken: string;
+    auth: Object;
+    displayName: string;
+    email: string;
+    emailVerified: boolean;
+    isAnonymous: boolean;
+    metadata: Object;
+    phoneNumber: string;
+    photoURL: string;
+    providerData: Array<Object>;
+    refreshToken: string;
+    tenantId: string;
+    uid: string;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+    // authUser: {
+    accessToken: "",
+    auth: {},
+    displayName: "",
+    email: "",
+    emailVerified: false,
+    isAnonymous: false,
+    metadata: {},
+    phoneNumber: "",
+    photoURL: "",
+    providerData: [{}],
+    refreshToken: "",
+    tenantId: "",
+    uid: "",
+    // },
 });
 
 export const AuthProvider: React.FC = ({
     children,
 }: React.EmbedHTMLAttributes<any>) => {
-    const [authUser, setAuthUser] = useState({});
+    const [authUser, setAuthUser] = useState<AuthContextType>();
 
     useEffect(() => {
-        const sub = onAuthStateChanged(auth, (user) => {
+        const sub = onAuthStateChanged(auth, (user: any) => {
             console.log(user);
 
             setAuthUser(user);
