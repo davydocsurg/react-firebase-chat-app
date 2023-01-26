5ebb35919ac4e1e2fe6cc9287b073d233a8df423
davydocsurg <davydocsurg@gmail.com>
2023-01-26 13:39:05 +0100

    rewrite auth context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,15 @@ import { config } from "../config";
 import { useAuthContext } from "../contexts";
 
 const Navbar = (): React.ReactElement => {
-    const res = useAuthContext();
-    console.log(res.authUser.displayName);
+    const authUser = useAuthContext();
+    const { displayName } = authUser?.authUser;
 
     return (
         <div className="navbar">
             <span className="logo">{config.appName}</span>
             <div className="user">
                 <img src={config.defaultPhoto} alt="" />
-                {/* <span>{displayName}</span> */}
+                <span>{displayName}</span>
                 <button onClick={() => console.log("auth")}>logout</button>
             </div>
         </div>
