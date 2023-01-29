4563f9931cdc7c6a2917a6e161d90617355cec00
davydocsurg <davydocsurg@gmail.com>
2023-01-29 22:16:20 +0100

    add user select feature #14

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,31 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    query,
+    serverTimestamp,
+    setDoc,
+    updateDoc,
+    where,
+} from "firebase/firestore";
 import React, { useState } from "react";
 import { db } from "../../firebase";
+import { useAuthContext } from "../contexts";
+
+type User = {
+    displayName: string;
+    email: string;
+    uid: string;
+    photoURL: string;
+};
 
 const Search = (): React.ReactElement => {
-    const [user, setUser] = useState<any>(null);
+    const [user, setUser] = useState<User | null>(null);
     const [username, setUsername] = useState("");
     const [err, setErr] = useState(false);
     const [queryArr, setQueryArr] = useState<any>([]);
+    const { uid, displayName, photoURL } = useAuthContext();
 
     const handleSearch = async () => {
         try {
@@ -20,8 +39,6 @@ const Search = (): React.ReactElement => {
                     username.charAt(0).toUpperCase() + username.slice(1)
                 )
             );
-
-            console.log(username.charAt(0).toUpperCase() + username.slice(1));
 
             const querySnapshots = await getDocs(term);
             setQueryArr(querySnapshots);
@@ -38,7 +55,40 @@ const Search = (): React.ReactElement => {
         e.code === "Enter" && handleSearch();
     };
 
-    const handleSelect = () => {};
+    const handleSelect = async () => {
+        // check if chat exists in firestore
+        const conversationId =
+            uid > user?.uid! ? uid + user?.uid : user?.uid + uid;
+        try {
+            const chatRef = doc(db, "chats", conversationId);
+            const res = await getDoc(chatRef);
+            if (!res.exists()) {
+                // if not, create new chat
+                await setDoc(doc(db, "chats", conversationId), {
+                    messages: [],
+                });
+
+                await updateDoc(doc(db, "userChats", uid), {
+                    [conversationId + ".userInfo"]: {
+                        uid: user?.uid,
+                        displayName: user?.displayName,
+                        photoURL: user?.photoURL,
+                    },
+                    [conversationId + ".date"]: serverTimestamp(),
+                });
+
+                await updateDoc(doc(db, "userChats", user?.uid!), {
+                    [conversationId + ".userInfo"]: {
+                        uid: uid,
+                        displayName: displayName,
+                        photoURL: photoURL,
+                    },
+                    [conversationId + ".date"]: serverTimestamp(),
+                });
+            }
+        } catch (error: unknown) {}
+        // if yes, go to chat
+    };
 
     return (
         <div className="search">
@@ -54,7 +104,7 @@ const Search = (): React.ReactElement => {
             {err && <span>User not found!</span>}
             {user && (
                 <div className="userChat" onClick={handleSelect}>
-                    <img src={user.photoURL} alt="" />
+                    <img src={user?.photoURL!} alt="" />
                     <div className="userChatInfo">
                         <span>{user.displayName}</span>
                     </div>
